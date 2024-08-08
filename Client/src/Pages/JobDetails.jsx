5c53b2bb8d79d5d867daf5acc1f6839f68a549c4
Aditya Prasannan <106433177+adi090@users.noreply.github.com>
2024-08-08 23:04:03 +0530

    Update JobDetails.jsx

--- Client/src/Pages/JobDetails.jsx
+++ Client/src/Pages/JobDetails.jsx
@@ -8,7 +8,7 @@ function JobDetails() {
   const [job, setJob] = useState(null);
 
   useEffect(() => {
-    fetch(`https://jobportal-backend-xcai.onrender.com/all-jobs/${id}`)
+    fetch(`http://localhost:5000/all-jobs/${id}`)
       .then(res => res.json())
       .then(data => setJob(data))
       .catch(error => console.error('Error fetching job details:', error));
