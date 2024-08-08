f061fba2a253884fab3c501f4fa1fa7c46db850a
Aditya Prasannan <106433177+adi090@users.noreply.github.com>
2024-08-08 16:07:01 +0530

    Update JobDetails.jsx

--- Client/src/Pages/JobDetails.jsx
+++ Client/src/Pages/JobDetails.jsx
@@ -8,7 +8,7 @@ function JobDetails() {
   const [job, setJob] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/all-jobs/${id}`)
+    fetch(`https://job-portal-api-olive.vercel.app/all-jobs/${id}`)
       .then(res => res.json())
       .then(data => setJob(data))
       .catch(error => console.error('Error fetching job details:', error));
