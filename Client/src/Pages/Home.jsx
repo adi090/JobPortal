68418669020317816db77688bb2e06b259d612f0
Aditya Prasannan <106433177+adi090@users.noreply.github.com>
2024-08-10 16:54:35 +0530

    Update Home.jsx

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
   const itemsPerPage=6;
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://jobportal-backend-lnsu.onrender.com/all-jobs/")
+    fetch("https://jobportal-backend-3aoq.onrender.com/all-jobs/")
       .then((res) => res.json())
       .then((data) => {
         setJobs(data);
